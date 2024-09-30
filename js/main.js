5b2da1144c60b85c2344dfa30fb4ee5370930a76
frinksdev <jf.ortega496@gmail.com>
2024-09-29 20:18:27 -0600

    Client: Listado de pokemon de la primera generación

--- js/main.js
+++ js/main.js
@@ -2,57 +2,67 @@ const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/generation/1/";
 
-/*for(let i = 1; i <= 151; i++){
-    fetch(URL + i)
-        .then((response) => response.json())
-        .then(data => mostrarPokemon(data))
-}*/ 
-
 fetch(URL)
     .then((response)=>response.json())
     .then(data => mostrarPokemon(data))
 
-function mostrarPokemon(poke) {
-    let tipos = poke.types.map((type) => `<p class="${type.type.name} type">${type.type.name}</p>`);
-    tipos = tipos.join('');
+function mostrarPokemon(poke){
 
-    let pokeId = poke.id.toString();
+    let pokes = poke['pokemon_species'];
+
+    let tipos = {};
+    poke.types.map(type =>{
+        tipos[type.name] = `<p class="${type.name} type">${type.name}</p>`;
+    });
+
+    /*let pokeId = poke.id.toString();
     if (pokeId.length === 1) {
         pokeId = "00" + pokeId;
     } else if (pokeId.length === 2) {
         pokeId = "0" + pokeId;
-    }
+    }*/
 
-    // Extract the latest cry URL from the API response
-    const cryLatestURL = poke.cries?.latest || '';
+    // @TODO: la api no restorna la info completa
+    pokes.forEach(async poke => {
+        const div = document.createElement("div");
+        div.classList.add("pokemon");
 
-    const div = document.createElement("div");
-    div.classList.add("pokemon");
-    div.innerHTML = `
-    <div class="pokemon">
-                <p class="pokemon-id-bg">#${pokeId}</p>
-                <div class="pokemon-img">
-                    <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon/'+poke.name)
+        let pokeDetail = await response.json();
+
+        let pokeTypes = pokeDetail.types.map(type => tipos[type.type.name]); 
+        
+        // Extract the latest cry URL from the API response
+        const cryLatestURL = poke.cries?.latest || '';
+
+        div.innerHTML = `
+        <div class="pokemon">
+                    <p class="pokemon-id-bg">${poke.id}</p>
+                    <div class="pokemon-img">
+                        <img src="${pokeDetail.sprites.other["official-artwork"].front_default}"
+                        alt="${pokeDetail.name}">
+                    </div>
+                    <div class="pokemon-info">
+                        <div class="name-bx">
+                            <p class="pokemon-id">${poke.id}</p>
+                            <h2 class="pokemon-name">${poke.name}</h2>
+                        </div>
+                        <div class="pokemon-types">
+                           ${pokeTypes}
+                        </div>
+                        <div class="pokemon-stats">
+                            <p class="stat">${pokeDetail.height / 10}ft</p>
+                            <p class="stat">${pokeDetail.weight / 10}lbs</p>
+                        </div>
+                        <div class="pokemon-cry">
+                            <button class="btn-cry" onclick="playCry(${poke.id})">Rugido</button>
+                            <audio id="cry-${poke.id}" src="${cryLatestURL}"></audio>
+                        </div>
+                    </div>
                 </div>
-                <div class="pokemon-info">
-                    <div class="name-bx">
-                        <p class="pokemon-id">#${pokeId}</p>
-                        <h2 class="pokemon-name">${poke.name}</h2>
-                    </div>
-                    <div class="pokemon-types">
-                       ${tipos}
-                    </div>
-                    <div class="pokemon-stats">
-                        <p class="stat">${poke.height / 10}m</p>
-                        <p class="stat">${poke.weight / 10}kg</p>
-                    </div>
-                    <div class="pokemon-cry">
-                        <button class="btn-cry" onclick="playCry(${poke.id})">Rugido</button>
-                        <audio id="cry-${poke.id}" src="${cryLatestURL}"></audio>
-                    </div>
-                </div>
-            </div>
-        `;
+            `;
+            listaPokemon.append(div);
+    });
     listaPokemon.append(div);
 }
 
