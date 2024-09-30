bc3c14fec25d0e0132aedbe8deafe7e8e5ec213a
Kid-Blue33 <103855234+KidBlue3@users.noreply.github.com>
2024-09-29 19:45:43 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const listaPokemon = document.querySelector("#listaPokemon");
+const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
 for(let i = 1; i <= 151; i++){
@@ -26,14 +27,14 @@ function mostrarPokemon(poke){
     div.classList.add("pokemon");
     div.innerHTML = `
     <div class="pokemon">
-                <p class="pokemon-id-bg">${pokeId}</p>
+                <p class="pokemon-id-bg">#${pokeId}</p>
                 <div class="pokemon-img">
                     <img src="${poke.sprites.other["official-artwork"].front_default}"
                     alt="${poke.name}">
                 </div>
                 <div class="pokemon-info">
                     <div class="name-bx">
-                        <p class="pokemon-id">${pokeId}</p>
+                        <p class="pokemon-id">#${pokeId}</p>
                         <h2 class="pokemon-name">${poke.name}</h2>
                     </div>
                     <div class="pokemon-types">
@@ -48,3 +49,25 @@ function mostrarPokemon(poke){
         `;
         listaPokemon.append(div);
 }
+
+botonesHeader.forEach(boton => boton.addEventListener("click",(event)=>{
+    const botonId = event.currentTarget.id;
+
+    listaPokemon.innerHTML = "";
+
+    for(let i = 1; i <= 151; i++){
+        fetch(URL + i)
+            .then((response) => response.json())
+            .then(data => {
+
+                if(botonId === "ver-todos"){
+                    mostrarPokemon(data);
+                }else{
+                    const tipos = data.types.map(type => type.type.name);
+                    if(tipos.some(tipo => tipo.includes(botonId))){
+                        mostrarPokemon(data);
+                    }
+                }
+            })
+    } 
+}))
