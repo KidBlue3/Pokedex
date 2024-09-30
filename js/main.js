e1f39c2e27a5be1cc15595df2611b09b5d62c33b
frinksdev <jf.ortega496@gmail.com>
2024-09-29 21:58:43 -0600

    Client: listando pokemon por tipo

--- js/main.js
+++ js/main.js
@@ -1,69 +1,73 @@
 const listaPokemon = document.querySelector("#listaPokemon");
+const listaTipos = document.querySelector(".nav-list");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/generation/1/";
+let URL_TYPES = "https://pokeapi.co/api/v2/type/";
 
-fetch(URL)
+(()=>{
+    fetch(URL)
     .then((response)=>response.json())
-    .then(data => mostrarPokemon(data))
+    .then(data => mostrarPokedex(data,"ver-todos"));
+})();
 
-function mostrarPokemon(poke){
-
-    let pokes = poke['pokemon_species'];
+function mostrarPokedex(pokedex, tipo){
 
     let tipos = {};
-    poke.types.map(type =>{
-        tipos[type.name] = `<p class="${type.name} type">${type.name}</p>`;
-    });
+    let pokes;
+    if(tipo === "ver-todos"){
+        pokes=pokedex['pokemon_species'];
+    } else {
+        pokes=pokedex['pokemon'];
+    }
 
-    /*let pokeId = poke.id.toString();
-    if (pokeId.length === 1) {
-        pokeId = "00" + pokeId;
-    } else if (pokeId.length === 2) {
-        pokeId = "0" + pokeId;
-    }*/
+    /*pokedex.types.map(type =>{
+        tipos[type.name] = `<p class="${type.id} type">${type.name}</p>`;
+    });*/
 
-    // @TODO: la api no restorna la info completa
     pokes.forEach(async poke => {
         const div = document.createElement("div");
         div.classList.add("pokemon");
 
-        let response = await fetch('https://pokeapi.co/api/v2/pokemon/'+poke.name)
+        console.log(poke);
+
+        let pkmName = poke.name || poke.pokemon.name;
+
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon/'+pkmName);
         let pokeDetail = await response.json();
 
-        let pokeTypes = pokeDetail.types.map(type => tipos[type.type.name]); 
+        //let pokeTypes = pokeDetail.types.map(type => tipos[type.type.name]); 
+        let pokeID = pokeDetail.id.toString().padStart(3, '0'); 
         
         // Extract the latest cry URL from the API response
-        const cryLatestURL = poke.cries?.latest || '';
+        const cryLatestURL = pokeDetail.cries?.latest || '';
 
         div.innerHTML = `
         <div class="pokemon">
-                    <p class="pokemon-id-bg">${poke.id}</p>
+                    <p class="pokemon-id-bg">${pokeDetail.id}</p>
                     <div class="pokemon-img">
                         <img src="${pokeDetail.sprites.other["official-artwork"].front_default}"
                         alt="${pokeDetail.name}">
                     </div>
                     <div class="pokemon-info">
                         <div class="name-bx">
-                            <p class="pokemon-id">${poke.id}</p>
-                            <h2 class="pokemon-name">${poke.name}</h2>
+                            <p class="pokemon-id">${pokeID}</p>
+                            <h2 class="pokemon-name">${pokeDetail.name}</h2>
                         </div>
                         <div class="pokemon-types">
-                           ${pokeTypes}
                         </div>
                         <div class="pokemon-stats">
                             <p class="stat">${pokeDetail.height / 10}ft</p>
                             <p class="stat">${pokeDetail.weight / 10}lbs</p>
                         </div>
                         <div class="pokemon-cry">
-                            <button class="btn-cry" onclick="playCry(${poke.id})">Rugido</button>
-                            <audio id="cry-${poke.id}" src="${cryLatestURL}"></audio>
+                            <button class="btn-cry" onclick="playCry(${pokeDetail.id})">Rugido</button>
+                            <audio id="cry-${pokeDetail.id}" src="${cryLatestURL}"></audio>
                         </div>
                     </div>
                 </div>
             `;
             listaPokemon.append(div);
     });
-    listaPokemon.append(div);
 }
 
 function playCry(pokeId) {
@@ -78,19 +82,14 @@ botonesHeader.forEach(boton => boton.addEventListener("click",(event)=>{
 
     listaPokemon.innerHTML = "";
 
-    for(let i = 1; i <= 151; i++){
-        fetch(URL + i)
-            .then((response) => response.json())
-            .then(data => {
+    let url = "https://pokeapi.co/api/v2/type/"+botonId;
+    if(botonId==="ver-todos"){
+        url = URL;
+    }
 
-                if(botonId === "ver-todos"){
-                    mostrarPokemon(data);
-                }else{
-                    const tipos = data.types.map(type => type.type.name);
-                    if(tipos.some(tipo => tipo.includes(botonId))){
-                        mostrarPokemon(data);
-                    }
-                }
-            })
-    } 
-}))
+    fetch(url)
+        .then((response)=>response.json())
+        .then(data => {
+            mostrarPokedex(data, botonId);
+        });
+}));
